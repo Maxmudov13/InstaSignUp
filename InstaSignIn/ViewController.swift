af655522828a308b83db8138deaa8ccc046e111b
Maxmudov Ilyos <maxmudovilyos2003@gmail.com>
2021-12-15 02:55:46 +0500

    Initial Commit

---
+++ InstaSignIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaSignIn
+//
+//  Created by user on 15/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
